fefa07955b119ebf49f581550ef3fc8d9d86975a
Tomas Oh <tomasoh@csu.fullerton.edu>
2024-10-22 20:21:19 -0700

    create slash command to get all courses

--- main.py
+++ main.py
@@ -15,16 +15,7 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="^", intents=intents, help_command=None)
 
-CANVAS_API_URL = "https://csufullerton.instructure.com"
-
-
-def get_canvas_assignments(class_id):
-    canvas = Canvas(CANVAS_API_URL, CANVAS_TOKEN)
-    course = canvas.get_course(class_id)
-    assignments = []
-    for assignment in course.get_assignments():
-        assignments.append(f"{assignment} DUE AT - {assignment.due_at}")
-    return "\n".join(assignments)
+CANVAS_API_URL = "https://canvas.instructure.com"
 
 
 # Embed generator to format embed message outpt
@@ -35,13 +26,49 @@ def createMessageEmbed(title: str, description: str):
     return embed
 
 
+# Function to get all Canvas courses associated with this user
+def get_canvas_courses():
+    canvas = Canvas(CANVAS_API_URL, CANVAS_TOKEN)
+    user = canvas.get_current_user()
+    course_list = []
+    for course in user.get_courses(enrollment_state="active"):
+        course_list.append(f"{course.name} ({course.id})")
+    return "\n".join(course_list)
+
+
+# Function to get all Canvas assignments given the course_id
+def get_canvas_assignments(course_id):
+    canvas = Canvas(CANVAS_API_URL, CANVAS_TOKEN)
+    course = canvas.get_course(course_id)
+    assignments = []
+    for assignment in course.get_assignments():
+        assignments.append(f"{assignment} DUE AT - {assignment.due_at}")
+    return "\n".join(assignments)
+
+
+# Register the canvas-courses slash command
+@bot.tree.command(
+    name="canvas-courses", description="Get Canvas courses from this user."
+)
+async def canvas_courses(interaction: discord.Interaction):
+    try:
+        courses = get_canvas_courses()
+        await interaction.response.send_message(
+            embed=createMessageEmbed(
+                title="Available Canvas Courses", description=courses
+            ),
+        )
+    except Exception as e:
+        await interaction.response.send_message(f"Error: {str(e)}", ephemeral=True)
+
+
 # Register the canvas-assignments slash command
 @bot.tree.command(
     name="canvas-assignments", description="Get Canvas assignments for a course."
 )
-async def canvas_assignments(interaction: discord.Interaction, class_id: int):
+async def canvas_assignments(interaction: discord.Interaction, course_id: int):
     try:
-        assignments = get_canvas_assignments(class_id)
+        assignments = get_canvas_assignments(course_id)
         await interaction.response.send_message(
             embed=createMessageEmbed(
                 title="Canvas Assignments", description=assignments
